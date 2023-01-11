536af01a95ead23c45134ae63039835667875a39
Adam Bullmer <psycodrumfreak@gmail.com>
2023-01-10 20:20:17 -0600

    feat(adambullmer): Add git command macros

--- users/adambullmer/adambullmer.c
+++ users/adambullmer/adambullmer.c
@@ -1,2 +1,23 @@
 #include QMK_KEYBOARD_H
 #include "adambullmer.h"
+
+const char * sendstring_commands[git_macro_keycodes_length] = {
+    "git init\n",
+    "git clone ",
+    "git config --global ",
+    "git add -p\n",
+    "git diff ",
+    "git reset\n",
+    "git rsq", // rebase -i --autosquash
+    "git branch ",
+    "git checkout ",
+    "git merge ",
+    "git remote add ",
+    "git fetch ",
+    "git pull ",
+    "git push ",
+    "git commit -m \"",
+    "git status\n",
+    "git log\n",
+    "git tree\n",
+};
